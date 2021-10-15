7d8f251efbb000df6530461326be9f86202fd070
Lalit Kumar <56152594+AdrashyaWarrior@users.noreply.github.com>
2021-10-15 20:26:35 +0530

    First Commit

--- Eloquent_Queries_Grouping.php
+++ Eloquent_Queries_Grouping.php
@@ -1,16 +1,13 @@
-    // Leads Index API
-    public function leads(Request $request)
+// Cutomers Index API
+    public function customer(Request $request)
     {
-        $search = $request->input('search') ?? '';
-        $perPage = $request->input('perPage') ?? 10;
-        $sortBy = $request->input('sortBy') ?? 'created_at';
-        $sortDirection = $request->input('sortDirection') ?? 'desc';
-
-        $leads = Lead::Query();
+        $customer = Lead::Query();
 
         // searching query
+        $search = $request->input('search') ?? '';
         if ($search != '')
-            $leads->where(function (EloquentBuilder $query) use ($search) {
+            // Grouping multiple orWheres() in a single where()
+            $customer->where(function (EloquentBuilder $query) use ($search) {
                 $query->whereHas('user', function (EloquentBuilder $q) use ($search) {
                     $q->where('name', 'like', '%' . $search . '%');
                 })->orWhere('full_name', 'like', '%' . $search . '%')
@@ -21,30 +18,35 @@
             });
 
         // filters
-        if ($request->input('support_id'))
-            $leads->where('user_id', $request->input('support_id'));
+        if ($request->input('user_id'))
+            $customer->where('user_id', $request->input('user_id'));
 
         if ($request->input('activity') == 'today')
-            $leads->whereDate('created_at', Carbon::today());
+            $customer->whereDate('created_at', Carbon::today());
 
         if ($request->input('status'))
-            $leads->where('status', ucfirst($request->input('status')));
+            $customer->where('status', ucfirst($request->input('status')));
 
         if ($request->input('daterange') != '') {
             $dates = explode('-', $request->input('daterange'));
-            $leads->whereBetween('date', [Carbon::createFromFormat('d/m/Y', $dates[0])->format('Y-m-d'), Carbon::createFromFormat('d/m/Y', $dates[1])->format('Y-m-d')]);
+            $customer->whereBetween('date', [Carbon::createFromFormat('d/m/Y', $dates[0])->format('Y-m-d'), Carbon::createFromFormat('d/m/Y', $dates[1])->format('Y-m-d')]);
         }
 
-        $leads->orderBy($sortBy, $sortDirection);
-
-        $total_items = $leads->count() ?? 0;
+        // Sorting
+        $sortBy = $request->input('sortBy') ?? 'created_at';
+        $sortDirection = $request->input('sortDirection') ?? 'desc';
+        $customer->orderBy($sortBy, $sortDirection);
+        
+        // Pagination
+        $total_items = $customer->count() ?? 0;
+        $perPage = $request->input('perPage') ?? 10;
         $total_pages = ceil($total_items / $perPage);
-        $leads = $leads->latest()->paginate($perPage);
-        $leads = LeadIndexResource::collection($leads);
+        $customer = $customer->latest()->paginate($perPage);
+        $customer = LeadIndexResource::collection($customer);
         $data = [
             'total_items' => $total_items,
             'total_pages' => $total_pages,
-            'leads' => $leads,
+            'customer' => $customer,
         ];
 
         return response()->json($data);
